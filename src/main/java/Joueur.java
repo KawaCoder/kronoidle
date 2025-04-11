b2354d1da2919b09786dfa580184b3b29518463a
Nathan <nathan.bendrell@etu.iut-tlse3.fr>
2025-04-11 16:54:47 +0200

    nahtan test 3

---
+++ src/main/java/Joueur.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Joueur {
+
+    // Constructeur
+    private void MyObject() {
+        // Attribut Map : clé = nom (String), valeur = valeur associée (Integer)
+        Map<String, Integer> ressources = new HashMap<>();
+    }
+
+    private int level;
+
+    private List<String> upgrade;
+
+    public static void buyupgrade() {
+
+    }
+
+
+}
