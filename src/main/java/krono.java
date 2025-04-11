236f4f1fbe8d61ced2fe2f608064012dfb7ddbcd
Titwo31 <titwo31@hotmail.com>
2025-04-11 16:04:26 +0200

    Doc Krono

--- src/main/java/krono.java
+++ src/main/java/krono.java
@@ -1,17 +1,33 @@
-public class krono {
+public class Krono {
     private int quantity;
 
+    /**
+     * Increases the quantity of Krono by the given amount
+     * @param quantity the amount to increase by
+     */
     public void addKrono(int quantity) {
         this.quantity += quantity;
     }   
+
 
     public void resetKrono() {
         this.quantity = 0;
     }
 
+    /**
+     * Subtracts the given quantity from the current quantity of krono.
+     *
+     * @param quantity the amount of krono to remove.
+     */
     public void removeKrono (int quantity) {
         this.quantity -= quantity;
     }
+
+    /**
+     * Returns the current quantity of krono.
+     *
+     * @return the current quantity of krono as an integer.
+     */
 
     public int getKrono() {
         return this.quantity;
