be243ce39b1aa0c2e0d165772380bacaa4265ea5
KawaCoder <KawaCoder@duck.com>
2025-04-17 23:04:51 +0200

    ajout item spécial

--- src/main/java/ItemLoader.java
+++ src/main/java/ItemLoader.java
@@ -9,6 +9,7 @@ public class ItemLoader {
         items.add(new Item("Animal Trap", "A primitive contraption to catch animals.", 10, 0.005));
         items.add(new Item("Stone Tools", "Simple tools made from stone.", 5, 0.002));
         items.add(new Item("Tribal Hut", "A temporary shelter for the tribe.", 20, 0.01));
+        items.add(new Item("Le gros paf à nathan", "a huge weapon for the tribe", 69, 420));
 
         return items;
     }
