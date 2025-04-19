55990b5902db12c3bcbf863f13c178d5dbd65fd0
Nathan <nathan.bendrell@etu.iut-tlse3.fr>
2025-04-19 23:11:53 +0200

    Merge branch 'master' of https://github.com/KawaCoder/kronoidle

--- src/main/java/ItemLoader.java
+++ src/main/java/ItemLoader.java
@@ -6,10 +6,11 @@ public class ItemLoader {
         List<Item> items = new ArrayList<>();
 
         items.add(new Item("Campfire", "A cozy place where the tribe can meet and rest.", 1, 0.001));
-        items.add(new Item("Animal Trap", "A primitive contraption to catch animals.", 10, 0.005));
         items.add(new Item("Stone Tools", "Simple tools made from stone.", 5, 0.002));
+        items.add(new Item("Animal Trap", "A primitive contraption to catch animals.", 10, 0.005));
         items.add(new Item("Tribal Hut", "A temporary shelter for the tribe.", 20, 0.01));
         items.add(new Item("Le gros paf à nathan", "a huge weapon for the tribe", 69, 420));
+        items.add(new Item("Magnaud le gros", "Be careful with this one", 1000, 666));
 
         return items;
     }
--- src/main/java/Upgrade.java
+++ src/main/java/Upgrade.java
@@ -6,12 +6,21 @@
 public class Upgrade {
     private String name;
     private int cost;
+    private int effect;
+    private boolean available;
 
     /**
      * Checks if the upgrade is available based on the player's Krono count.
      */
     public boolean isAvailable(KronoCounter k) {
         return(k.getKrono() >= this.cost);
+    }
+
+    public Upgrade(String name, int cost, int effect) {
+        this.name = name;
+        this.cost = cost;
+        this.effect = effect;
+        this.available = true;
     }
 
 }
